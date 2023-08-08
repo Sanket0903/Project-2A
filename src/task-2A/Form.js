169a4329c559f6af6facb9731ae4e3f302395b28
Sanket0903 <138984850+Sanket0903@users.noreply.github.com>
2023-08-08 21:06:14 +0530

    bootstrap

--- src/task-2A/Form.js
+++ src/task-2A/Form.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './Form.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Form() {
   const [selectedOption, setSelectedOption] = useState('');
@@ -37,57 +37,70 @@ function Form() {
   };
 
   return (
-    <form onSubmit={handleSubmit} className='container'>
-      <div className='wrapper'>
-        <label className='firstName'>
-          First Name<br />
-          <input
-            type='text'
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
-        </label>
-        <label className='lastName'>
-          Last Name<br />
-          <input
-            type='text'
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
-        </label>
-      </div>
-      <div className='selectWrapper'>
-        <select value={selectedOption} onChange={handleChange}>
-          <option value="">Choose option</option>
-          <option value="number">Phone Number</option>
-          <option value="email">Email Address</option>
-        </select>
-        {selectedOption === 'number' && (
-          <div>
-            <label style={{ margin: "20px" }}>Phone Number</label>
-            <input
-              type='number'
-              value={phoneNumber}
-              onChange={(e) => setPhoneNumber(e.target.value)}
-            />
+    <div className="container">
+      <div className='row'>
+        <div className="card mt-5">
+          <div className="card-body">
+            <form onSubmit={handleSubmit}>
+              <div className='row'><div className="col-6 mb-3">
+                <label for='firstName' className="form-label">First Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id='firstName'
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
+              <div className="col-6 mb-5">
+                <label for="lastName" className="form-label">Last Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  id='lastName'
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div></div>
+              
+              <div className="col-4 mb-3">
+                <select className="form-select" value={selectedOption} onChange={handleChange}>
+                  <option value="">Choose option</option>
+                  <option value="number">Phone Number</option>
+                  <option value="email">Email Address</option>
+                </select>
+                {selectedOption === 'number' && (
+                  <div className="mt-3">
+                    <label className="form-label">Phone Number</label>
+                    <input
+                      type="number"
+                      className="form-control"
+                      value={phoneNumber}
+                      onChange={(e) => setPhoneNumber(e.target.value)}
+                    />
+                  </div>
+                )}
+                {selectedOption === 'email' && (
+                  <div className="mt-3">
+                    <label className="form-label">Email Address</label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
+                  </div>
+                )}
+                {isFormComplete === false && (
+                  <p className="mt-3 text-danger">Please fill all details.</p>
+                )}
+                <button type="submit" className="btn btn-primary mt-3">Submit</button>
+              </div>
+            </form>
           </div>
-        )}
-        {selectedOption === 'email' && (
-          <div>
-            <label style={{ margin: "20px" }}>Email Address</label>
-            <input
-              type='email'
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-        )}
-        {isFormComplete === false && (
-          <p style={{ color: 'red' }}>Please fill all details.</p>
-        )}
-        <button type='submit'>Submit</button>
-      </div>
-    </form>
+        </div>
+        </div>
+    </div>
   );
 }
 
